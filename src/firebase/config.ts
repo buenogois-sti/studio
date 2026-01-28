c1e6ece71a07583cd84395b942766e05ddbec394
Alan Bueno de Gois <dr.alan@buenogoisadvogado.com.br>
2026-01-28 12:34:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7080106838-23904",
+  "appId": "1:1052927104977:web:0da5cf6e5a80716920b2f0",
+  "apiKey": "AIzaSyCHdAgx_Nq8_8LEE9waWboDfENosgtXE0Q",
+  "authDomain": "studio-7080106838-23904.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1052927104977"
+};
